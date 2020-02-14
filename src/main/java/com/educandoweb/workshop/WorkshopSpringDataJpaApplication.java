3ce9065a670e7d7fd5edb72e13ee6963bbb1f2cd
Mateus <mateuslima020@outlook.com>
2020-02-14 00:52:48 -0200

    Project created

---
+++ src/main/java/com/educandoweb/workshop/WorkshopSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.educandoweb.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringDataJpaApplication.class, args);
+	}
+
+}
