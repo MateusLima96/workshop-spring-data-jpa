7028859540b617ed492572be8ee6d1a75534d635
Mateus <mateuslima020@outlook.com>
2020-03-11 22:06:03 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/workshop/config/TestConfig.java
+++ src/main/java/com/educandoweb/workshop/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.educandoweb.workshop.entities.Category;
 import com.educandoweb.workshop.entities.Order;
 import com.educandoweb.workshop.entities.OrderItem;
+import com.educandoweb.workshop.entities.Payment;
 import com.educandoweb.workshop.entities.Product;
 import com.educandoweb.workshop.entities.User;
 import com.educandoweb.workshop.entities.enums.OrderStatus;
@@ -65,6 +66,10 @@ public class TestConfig implements CommandLineRunner {
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1= new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 		
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
